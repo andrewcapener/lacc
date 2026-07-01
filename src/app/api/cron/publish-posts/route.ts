52666a5af343e819d6242d039f39a805755e6ab3
andrewcapener <andrew.c.capener@gmail.com>
2026-07-01 10:33:56 -0700

    Fix cron route: add nodejs runtime export to resolve ESM module error

--- src/app/api/cron/publish-posts/route.ts
+++ src/app/api/cron/publish-posts/route.ts
@@ -1,3 +1,5 @@
+export const runtime = 'nodejs'
+
 import { NextRequest, NextResponse } from 'next/server'
 import { getKeywordOpportunities } from '@/lib/search-console'
 import { generatePost } from '@/lib/content-generator'
